2770212ddcaa1e8958d87e9d9e470ee1cccda20b
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2025-07-01 14:07:57 +0100

    done

--- krosack-site/next.config.ts
+++ krosack-site/next.config.ts
@@ -1,19 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
-    domains: ['raw.githubusercontent.com'],
-  },
+  //  images: {
+  //   domains: ['raw.githubusercontent.com'],
+  // },
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push({
-        'html2pdf.js': 'html2pdf.js',
-      });
-    }
-
-    return config;
-  },
+  if (isServer) {
+    config.externals = [...(config.externals || []), 'html2pdf.js'];
+  }
+  return config;
+}
 };
 
 export default nextConfig;
